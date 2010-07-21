1bfddebaa2999a143bef93bd29fa81db54785550
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2010-07-21 21:48:36 +0000

    Empty class will eventually manage the authentication with Wiser Path online.

---
+++ WiserPathMobile/src/path/wiser/mobile/LoginManager.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package path.wiser.mobile;
+
+/**
+ * @author andrew nisbet
+ * 
+ *         This class tests if the user is a valid Wiser Path user
+ *         and manages the user login and authentication with the web site.
+ * 
+ * 
+ */
+public class LoginManager
+{
+
+}
