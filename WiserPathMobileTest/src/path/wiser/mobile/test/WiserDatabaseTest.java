c7e37ac9b7295d31a0cddb9ea8a586ced5b3e5c4
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2010-08-06 19:00:14 +0000

    Fixed: don't use @Test because it creates a new instance of the class and none of the DB construction has happened.

--- WiserPathMobileTest/src/path/wiser/mobile/test/WiserDatabaseTest.java
+++ WiserPathMobileTest/src/path/wiser/mobile/test/WiserDatabaseTest.java
@@ -1,56 +1,66 @@
-/**
- * 
- */
 package path.wiser.mobile.test;
 
-import static org.junit.Assert.assertNotNull;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import path.wiser.mobile.db.WiserDatabase;
-
-import android.app.Activity;
-import android.os.Bundle;
 import android.test.AndroidTestCase;
 import android.util.Log;
-import android.widget.TextView;
+import junit.framework.TestCase;
 
-/**
- * @author andrewnisbet
- * 
- */
-public class WiserDatabaseTest extends AndroidTestCase
-{
+//public class WiserDatabaseTest extends AndroidTestCase
+//{
+//	public final static String	TAG	= "WiserDBTest";
+//
+//	private WiserDatabase		wdb	= null;
+//
+//
+//	/**
+//	 * Test method for
+//	 * {@link path.wiser.mobile.db.WiserDatabase#WiserDatabase(android.content.Context)}
+//	 * .
+//	 */
+//	@Test
+//	public void testWiserDatabase()
+//	{
+//		WiserDatabase db = new WiserDatabase( null );
+//		assertNotNull( db );
+//		db.open();
+//		Log.i( TAG, "Print something will ya?" );
+//	}
+//
+//	/**
+//	 * @throws java.lang.Exception
+//	 */
+//	@Before
+//	public void setUp() throws Exception
+//	{
+//		wdb = new WiserDatabase( this.getContext() );
+//		assertNotNull( wdb );
+//		wdb.close();
+//		
+//	}
+//
+//}
+
+
+
+public class WiserDatabaseTest extends AndroidTestCase {
+	
 	public final static String	TAG	= "WiserDBTest";
-
 	private WiserDatabase		wdb	= null;
-
-
-	/**
-	 * Test method for
-	 * {@link path.wiser.mobile.db.WiserDatabase#WiserDatabase(android.content.Context)}
-	 * .
-	 */
-	@Test
-	public void testWiserDatabase()
-	{
-		WiserDatabase db = new WiserDatabase( null );
-		assertNotNull( db );
-		db.open();
-		Log.i( TAG, "Print something will ya?" );
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
 	@Before
-	public void setUp() throws Exception
-	{
+	protected void setUp() throws Exception {
+		super.setUp();
 		wdb = new WiserDatabase( this.getContext() );
 		assertNotNull( wdb );
+	}
+	
+	@After
+	protected void tearDown() {
 		wdb.close();
-		
 	}
 
 }
