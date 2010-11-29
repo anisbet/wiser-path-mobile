e7c5c54baecbbaa976bc7e7407dfd7cb983d8f87
bhavani.sudhireddy <bhavani.sudhireddy@gmail.com>
2010-11-29 20:38:58 +0000

    added a comment to check commit option

--- WiserPathMobile/src/path/wiser/mobile/WiserPathMobile.java
+++ WiserPathMobile/src/path/wiser/mobile/WiserPathMobile.java
@@ -8,11 +8,6 @@ import path.wiser.mobile.ui.PointOfInterestActivity;
 import path.wiser.mobile.ui.SettingsActivity;
 import path.wiser.mobile.ui.TraceActivity;
 import path.wiser.mobile.ui.WiserPathActivity;
-import android.app.TabActivity;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.widget.TabHost;
 
 /**
  * @author andrew nisbet
@@ -24,7 +19,7 @@ public class WiserPathMobile extends TabActivity
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
-		setContentView( R.layout.main );
+		setContentView( R.layout.main ); // bhavani
 
 		Resources res = getResources(); // Resource object to get Drawables
 		TabHost tabHost = getTabHost(); // The activity TabHost
@@ -34,40 +29,25 @@ public class WiserPathMobile extends TabActivity
 		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass( this, TraceActivity.class );
 		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost.newTabSpec( "Trace" )
-			.setIndicator( "Trace", res.getDrawable( R.drawable.ic_tab_trace ) )
-			.setContent( intent );
+		spec = tabHost.newTabSpec( "Trace" ).setIndicator( "Trace", res.getDrawable( R.drawable.ic_tab_trace ) ).setContent( intent );
 		tabHost.addTab( spec );
 
 		// Do the same for the other tabs
 		intent = new Intent().setClass( this, PointOfInterestActivity.class );
 		// Initialize a TabSpec for each tab and add it to the TabHost
-		spec = tabHost.newTabSpec( "POI" )
-			.setIndicator( "POI", res.getDrawable( R.drawable.ic_tab_poi ) )
-			.setContent( intent );
+		spec = tabHost.newTabSpec( "POI" ).setIndicator( "POI", res.getDrawable( R.drawable.ic_tab_poi ) ).setContent( intent );
 		tabHost.addTab( spec );
 
 		intent = new Intent().setClass( this, WiserPathActivity.class );
-		spec = tabHost
-			.newTabSpec( "Wiser Path" )
-			.setIndicator( "Wiser Path",
-				res.getDrawable( R.drawable.ic_tab_witp ) ).setContent( intent );
+		spec = tabHost.newTabSpec( "Wiser Path" ).setIndicator( "Wiser Path", res.getDrawable( R.drawable.ic_tab_witp ) ).setContent( intent );
 		tabHost.addTab( spec );
 
 		intent = new Intent().setClass( this, IncidentActivity.class );
-		spec = tabHost
-			.newTabSpec( "Incident" )
-			.setIndicator( "Incident",
-				res.getDrawable( R.drawable.ic_tab_incident ) )
-			.setContent( intent );
+		spec = tabHost.newTabSpec( "Incident" ).setIndicator( "Incident", res.getDrawable( R.drawable.ic_tab_incident ) ).setContent( intent );
 		tabHost.addTab( spec );
 
 		intent = new Intent().setClass( this, SettingsActivity.class );
-		spec = tabHost
-			.newTabSpec( "Options" )
-			.setIndicator( "Options",
-				res.getDrawable( R.drawable.ic_tab_options ) )
-			.setContent( intent );
+		spec = tabHost.newTabSpec( "Options" ).setIndicator( "Options", res.getDrawable( R.drawable.ic_tab_options ) ).setContent( intent );
 		tabHost.addTab( spec );
 
 		tabHost.setCurrentTab( 2 );
