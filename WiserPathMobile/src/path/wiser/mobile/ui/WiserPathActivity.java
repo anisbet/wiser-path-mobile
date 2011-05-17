4d52fad63138a1043978aa41be21d24ae6e6156b
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-05-17 22:33:02 +0000

    Make this class abstract to prevent instantiation.

--- WiserPathMobile/src/path/wiser/mobile/ui/WiserPathActivity.java
+++ WiserPathMobile/src/path/wiser/mobile/ui/WiserPathActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class WiserPathActivity extends Activity
+public abstract class WiserPathActivity extends Activity
 {
 	/** Called when the activity is first created. */
 	@Override
