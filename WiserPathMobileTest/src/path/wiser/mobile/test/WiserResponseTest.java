f7ecc2f7eff1d27814a5e6d60facd5f387efd2a3
nisbetorama <nisbetorama@gmail.com>
2011-05-25 21:36:03 +0000

    New test for the Post object.

--- WiserPathMobileTest/src/path/wiser/mobile/test/WiserResponseTest.java
+++ WiserPathMobileTest/src/path/wiser/mobile/test/WiserResponseTest.java
@@ -3,6 +3,8 @@
  */
 package path.wiser.mobile.test;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,7 +15,7 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import path.wiser.mobile.services.WiserResponse;
+import path.wiser.mobile.services.Post;
 
 /**
  * @author andrewnisbet
@@ -21,26 +23,57 @@ import path.wiser.mobile.services.WiserResponse;
  */
 public class WiserResponseTest extends TestCase {
 
-	/**
-	 * Test method for {@link path.wiser.mobile.services.WiserResponse#WiserResponse(java.net.URLConnection, java.lang.String)}.
-	 */
 	@Test
-	public void testWiserResponse() {
-		
+	public void testNullPost() {
 		String dataToPost = "name=" + "anisbet" + "&pass=" + "leader1" + "&form_id=user_login";
-		URL url;
+		URL url = null;
+		System.out.print( "An error message here is normal: " );
 		try {
-			url = new URL( "http://wiserpath-dev.bus.ualberta.ca/user/login" );
-			URLConnection connection = url.openConnection();
-			WiserResponse response = new WiserResponse( connection, URLEncoder.encode( dataToPost, "UTF-8" ) );
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//url = new URL( "http://wiserpath-dev.bus.ualberta.ca/user/login" );
+			dataToPost = URLEncoder.encode( dataToPost, "UTF-8" );
+			Post response = new Post( url );
+			response.post( dataToPost );
+			System.out.println( "the return code was: '" + response.getReturnCode() + "'" );
+		} catch (Exception e) {
+			assertTrue( e instanceof NullPointerException );
+		}
+	}
+	
+	@Test
+	public void testInvalidURL() {
+		String dataToPost = "name=" + "anisbet" + "&pass=" + "leader1" + "&form_id=user_login";
+		URL url = null;
+		Post response = null;
+		System.out.print( "An error message here is normal: " );
+		try {
+			url = new URL( "http://www/user/login" );
+			dataToPost = URLEncoder.encode( dataToPost, "UTF-8" );
+			response = new Post( url );
+			response.post( dataToPost );
+			System.out.println( "the return code was: '" + response.getReturnCode() + "'" );
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		assertTrue( response.getReturnCode() == 400 );
+	}
+	
+	@Test
+	public void testValidPost() {
+		String dataToPost = "name=" + "anisbet" + "&pass=" + "leader1" + "&form_id=user_login&op=log+in";
+		Post response = null;
+		URL url = null;
+		try {
+			url = new URL( "http://wiserpath-dev.bus.ualberta.ca/user/login" );
+			//dataToPost = URLEncoder.encode( dataToPost, "UTF-8" );
+			response = new Post(url);
+			System.out.println( "OUTPUT:" + response.post(dataToPost) );
+			
+//			System.out.println( "the return code was: '" + response.getReturnCode() + "'" );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertTrue( response.getReturnCode() == 200 );
+		System.out.println( ">>>" + response.getReturnCode() );
 	}
 
 }
