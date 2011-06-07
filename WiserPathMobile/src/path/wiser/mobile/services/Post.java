695f3107c56ef9544590c74af8d489f04cd3e6de
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-07 21:21:14 +0000

    No changes -- undid all changes.

--- WiserPathMobile/src/path/wiser/mobile/services/Post.java
+++ WiserPathMobile/src/path/wiser/mobile/services/Post.java
@@ -15,23 +15,23 @@ import java.net.URLConnection;
 public class Post
 {
 
-	private int response = -1; // unset
-	private WiserCookie wiserCookie = null;
-	private URLConnection connection = null;
-	
+	private int				response	= -1;	// unset
+	private WiserCookie		wiserCookie	= null;
+	private URLConnection	connection	= null;
 
 	/**
 	 * Requires an open connection.
+	 * 
 	 * @param connection
 	 */
 	public Post( URL url )
-	{ 
+	{
 		response = 400; // preset to error and reset when another code is available.
-		try 
+		try
 		{
 			connection = url.openConnection();
-		} 
-		catch ( Exception e ) 
+		}
+		catch (Exception e)
 		{
 			// Log.e( "Post:Error", "The URL is either NULL or there was an IO error." );
 			System.err.println( "Post error: Cannot connect because the URL is either NULL or there was an IO error." );
@@ -42,13 +42,13 @@ public class Post
 		// Specify the content type.
 		connection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded" );
 	}
-	
+
 	/**
 	 * @param data
 	 * @return String of the data returned from the post or null if there was none.
 	 */
 	@SuppressWarnings("deprecation")
-	public String post(  String data )
+	public String post( String data )
 	{
 		String result = new String();
 		connection.setDoOutput( true );
@@ -58,7 +58,7 @@ public class Post
 		connection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded" );
 		DataOutputStream outBoundStream = null;
 		DataInputStream inBoundStream = null;
-		try 
+		try
 		{
 			outBoundStream = new DataOutputStream( connection.getOutputStream() );
 			outBoundStream.writeBytes( data );
@@ -67,12 +67,12 @@ public class Post
 			// Get response data.
 			inBoundStream = new DataInputStream( connection.getInputStream() );
 			String temp;
-			while ( (temp = inBoundStream.readLine()) != null )
+			while (( temp = inBoundStream.readLine() ) != null)
 			{
 				result += temp + "\n";
 			}
 		}
-		catch ( IOException e ) // can be IOException or EOFException when the stream has finished reading.
+		catch (IOException e) // can be IOException or EOFException when the stream has finished reading.
 		{
 			// Log.e( "Post:Error", "Could not post because of an IO error." );
 			System.err.println( "Post error: Could not post because of an IO error. Perhaps the URL is incorrect." );
@@ -80,28 +80,29 @@ public class Post
 		}
 		finally
 		{
-			try 
+			try
 			{
 				inBoundStream.close();
-				readHTTPHeader(connection);
+				readHTTPHeader( connection );
 				return result.toString();
-			} 
-			catch ( Exception e ) 
+			}
+			catch (Exception e)
 			{
-				response = 400; // this is normal if the inbound stream wasn't opened or if there was an exception during closing.
+				response = 400; // this is normal if the inbound stream wasn't opened or if there was an exception
+								// during closing.
 			}
 		}
 		return null;
 	}
-	
 
-	/** 
+	/**
 	 * Reads the header of a connection then can be queried for values.
+	 * 
 	 * @param connection
 	 */
-	private void readHTTPHeader(URLConnection connection) 
+	private void readHTTPHeader( URLConnection connection )
 	{
-		for ( int i = 0; ; i++ )
+		for (int i = 0;; i++)
 		{
 			String name = connection.getHeaderFieldKey( i );
 			String value = connection.getHeaderField( i );
@@ -112,20 +113,19 @@ public class Post
 			if (name == null)
 			{
 				// this retrieves out the HTTP version number and the status code.
-				String[] values = value.split(" ");
+				String[] values = value.split( " " );
 				response = Integer.parseInt( values[1] );
 			}
 			else
 			{
-//				System.out.println( name + "=" + value );
-				if ( name.equalsIgnoreCase( "Set-Cookie" ))
+				// System.out.println( name + "=" + value );
+				if (name.equalsIgnoreCase( "Set-Cookie" ))
 				{
 					wiserCookie = new WiserCookie( value );
 				}
 			}
-		}		
+		}
 	}
-
 
 	/**
 	 * @return reponse code from returned header.
@@ -138,7 +138,7 @@ public class Post
 	/**
 	 * @return the wiserCookie
 	 */
-	public WiserCookie getWiserCookie() 
+	public WiserCookie getWiserCookie()
 	{
 		return wiserCookie;
 	}
