693531b01734157e322079df98f18b55ad9fb96d
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-07 20:28:46 +0000

    Login method implemented and tested.

--- WiserPathMobile/src/path/wiser/mobile/WiserPathMobile.java
+++ WiserPathMobile/src/path/wiser/mobile/WiserPathMobile.java
@@ -3,16 +3,21 @@
  */
 package path.wiser.mobile;
 
+import path.wiser.mobile.services.Credential;
+import path.wiser.mobile.services.HTTPService;
 import path.wiser.mobile.ui.IncidentActivity;
 import path.wiser.mobile.ui.PointOfInterestActivity;
 import path.wiser.mobile.ui.SettingsActivity;
 import path.wiser.mobile.ui.TraceActivity;
 import path.wiser.mobile.ui.WiserPathActivity;
+import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 /**
  * @author andrew nisbet
@@ -21,14 +26,19 @@ import android.widget.TabHost;
 public class WiserPathMobile extends TabActivity
 {
 
+	private static final int	PREFS_TAB		= 4;
+	private static final int	POI_TAB			= 1;
+	private Credential			loginCredential	= null;
+
 	public void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
-		setContentView( R.layout.main ); // bhavani
+		setContentView( R.layout.main );
 
 		Resources res = getResources(); // Resource object to get Drawables
 		TabHost tabHost = getTabHost(); // The activity TabHost
-		TabHost.TabSpec spec; // Resusable TabSpec for each tab
+		TabHost.TabSpec spec; // Reusable TabSpec for each tab
+
 		Intent intent; // Reusable Intent for each tab
 
 		// Create an Intent to launch an Activity for the tab (to be reused)
@@ -55,6 +65,41 @@ public class WiserPathMobile extends TabActivity
 		spec = tabHost.newTabSpec( "Options" ).setIndicator( "Options", res.getDrawable( R.drawable.ic_tab_options ) ).setContent( intent );
 		tabHost.addTab( spec );
 
-		tabHost.setCurrentTab( 2 );
+		// Try to login.
+		// Determine which tab should be shown first -- we will also have to account for new users registering.
+		if (login() == false)
+		{
+			tabHost.setCurrentTab( WiserPathMobile.PREFS_TAB );
+		}
+		else
+		{
+			tabHost.setCurrentTab( WiserPathMobile.POI_TAB );
+		}
 	}
+
+	/**
+	 * @return Credential object with either the username password from
+	 *         preferences or the default values from Credential.
+	 */
+	private boolean login()
+	{
+		SharedPreferences myPrefs = this.getSharedPreferences( SettingsActivity.PREFS_FILE_NAME, Activity.MODE_PRIVATE );
+		// the user name will be set to what ever is in the private preference file and if there isn't one
+		// will be set to the default value (found in Credential.java).
+		String userName = myPrefs.getString( Credential.USER_NAME, Credential.DEFAULT_USER_NAME );
+		String password = myPrefs.getString( Credential.PASSWORD, Credential.DEFAULT_PASSWORD );
+
+		// TODO keep this for saving POI::Bitmap bm =
+		// BitmapFactory.decodeFile( "/data/misc/wallpaper/" + bmpName
+		// );
+		// TODO delete this for production version.
+		Toast.makeText( this, "User name and password have been read from preferences:" + "userName=" + userName + " password:" + password,
+			Toast.LENGTH_LONG ).show();
+		// Now try and make a connection and login.
+		HTTPService httpService = new HTTPService();
+		this.loginCredential = new Credential( userName, password );
+
+		return httpService.login( this.loginCredential );
+	}
+
 }
