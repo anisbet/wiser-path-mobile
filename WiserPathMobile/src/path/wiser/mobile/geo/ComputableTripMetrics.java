6aaa709e162403462ebd9821b4a6257b04ce8466
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-15 21:45:40 +0000

    Added this interface so a standard set of methods can be called for trace objects and the computer that it uses.

---
+++ WiserPathMobile/src/path/wiser/mobile/geo/ComputableTripMetrics.java
@@ -0,0 +1,20 @@
+package path.wiser.mobile.geo;
+
+public interface ComputableTripMetrics
+{
+
+	public String getSpeed();
+
+	public String getDistance();
+
+	public String getLatitude();
+
+	public String getLongtitude();
+
+	public String getEllapseTime();
+
+	public String getDirection();
+
+	public String getPace();
+
+}
