2dcc0fe01e2eded8988a13a5e595fbeb630f6297
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-15 21:46:18 +0000

    Roughly implemented the calculations that can be done on trace locations.

--- WiserPathMobile/src/path/wiser/mobile/geo/TraceComputer.java
+++ WiserPathMobile/src/path/wiser/mobile/geo/TraceComputer.java
@@ -23,13 +23,88 @@ import android.location.Location;
  * @author andrewnisbet
  * 
  */
-public class TraceComputer
+public class TraceComputer implements ComputableTripMetrics
 {
-	private static Units	units;
+	private Vector<Location>	locations	= null;
 
-	public TraceComputer( Units u, Vector<Location> locations )
+	public TraceComputer( Vector<Location> locations )
 	{
-		units = u;
+		this.locations = locations;
+	}
+
+	@Override
+	public String getSpeed()
+	{
+		float value = this.locations.lastElement().getSpeed();
+		return String.valueOf( value ) + Units.speed();
+	}
+
+	@Override
+	public String getDistance()
+	{
+		double value = this.locations.firstElement().distanceTo( this.locations.lastElement() );
+		// or should this be the total from point to point.
+		return String.valueOf( value ) + Units.distance();
+	}
+
+	@Override
+	public String getLatitude()
+	{
+		double value = this.locations.lastElement().getLatitude();
+		// do we need to convert to degrees minutes seconds.
+		return String.valueOf( value ) + Units.coordinate( value, true );
+	}
+
+	@Override
+	public String getLongtitude()
+	{
+		double value = this.locations.lastElement().getLongitude();
+		// do we need to convert to degrees minutes seconds.
+		return String.valueOf( value ) + Units.coordinate( value, false );
+	}
+
+	@Override
+	public String getEllapseTime()
+	{
+		long value = this.locations.lastElement().getTime() - this.locations.firstElement().getTime();
+		// this will now contain the difference in milliseconds.
+		return String.valueOf( value ) + Units.time();
+	}
+
+	@Override
+	public String getDirection()
+	{
+		float value = this.locations.lastElement().getBearing();
+		return String.valueOf( value ) + Units.bearing();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see path.wiser.mobile.geo.ComputableTripMetrics#getPace()
+	 */
+	@Override
+	public String getPace()
+	{
+		// pace is the difference in speed from the last location.
+		float pace = 0.0f;
+
+		if (this.locations.size() > 0)
+		{
+			if (this.locations.size() > 1)
+			{
+				// get speed from the second last element and subtract the speed at the last element -- it may be neg or
+				// 0 or pos.
+				pace = this.locations.get( this.locations.size() - 2 ).getSpeed() - this.locations.lastElement().getSpeed();
+			}
+			else
+			// there is only one element so return its speed because the delta from 0.0 is the current speed.
+			{
+				pace = this.locations.firstElement().getSpeed();
+			}
+		}
+		// if no locations recorded the pace is 0.0.
+		return String.valueOf( pace ) + Units.pace();
 	}
 
 }
