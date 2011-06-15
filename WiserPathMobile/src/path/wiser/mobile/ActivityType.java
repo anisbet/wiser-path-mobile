e4b67c7711c2bba17bee8e0677f7d863e7125300
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-15 21:44:45 +0000

    Created Units type for computing the affixes of strings to display in the tabs.

---
+++ WiserPathMobile/src/path/wiser/mobile/ActivityType.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package path.wiser.mobile;
+
+/**
+ * The types of activities controls things like if we use altitude and how frequently we
+ * we want the GPS to record points.
+ * 
+ * @author andrewnisbet
+ * 
+ */
+public enum ActivityType
+{
+	WALK, RUN, HIKE, // uses altitude
+	BIKE,
+	DRIVE,
+	FLY
+	// uses altitude
+}
--- WiserPathMobile/src/path/wiser/mobile/Units.java
+++ WiserPathMobile/src/path/wiser/mobile/Units.java
@@ -7,7 +7,104 @@ package path.wiser.mobile;
  * @author andrewnisbet
  * 
  */
-public enum Units
+public class Units
 {
-	METRIC, IMPERIAL
+
+	private static MeasurementSystem	measurementSystem;
+	private static ActivityType			activityType;
+
+	private Units()
+	{
+
+	}
+
+	public enum MeasurementSystem
+	{
+		METRIC, IMPERIAL
+	}
+
+	public static void setMeasurementSystem( MeasurementSystem system )
+	{
+		measurementSystem = system;
+	}
+
+	public static void setActivityType( ActivityType type )
+	{
+		activityType = type;
+	}
+
+	/**
+	 * @return Standard unit of measure for speed based on measurement system.
+	 */
+	public static String speed()
+	{
+		switch (measurementSystem)
+		{
+		case IMPERIAL:
+			return " MPH";
+		default:
+			return " KPH";
+		}
+	}
+
+	public static double convertToImperialDistance( double measure )
+	{
+		// TODO compute imperial distance from metric value.
+		return 0.0;
+	}
+
+	/**
+	 * @return
+	 */
+	public static String distance()
+	{
+		switch (measurementSystem)
+		{
+		case IMPERIAL:
+			return " mi";
+		default:
+			return " kms";
+		}
+	}
+
+	/**
+	 * @param value the value the user will show on the screen.
+	 * @param isLatitude if treu will put E/W and if false will put N/S as appropriate.
+	 * @return N, S, E, or W.
+	 */
+	public static String coordinate( double value, boolean isLatitude )
+	{
+		// TODO Convert to the desired location and append type.
+		return " deg N";
+	}
+
+	/**
+	 * @return Units of time -- seconds.
+	 */
+	public static String time()
+	{
+		return " sec";
+	}
+
+	/**
+	 * @return units for bearing.
+	 */
+	public static String bearing()
+	{
+		// TODO Consults Arie about what this would be.
+		return " deg";
+	}
+
+	public static String pace()
+	{
+		// TODO also account for activity type for more useful units.
+		switch (measurementSystem)
+		{
+		case IMPERIAL:
+			return " feet/sec";
+		default:
+			return " m/s";
+		}
+	}
+
 }
