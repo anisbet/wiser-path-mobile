7a5561f52947b2f6ac2a9d6d3d610dc8a5c180b6
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-17 22:50:03 +0000

    Created and tested this class and interface.

---
+++ WiserPathMobile/src/path/wiser/mobile/BlogMVC.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package path.wiser.mobile;
+
+import path.wiser.mobile.geo.Blog;
+import path.wiser.mobile.ui.PointOfInterestActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * @author andrewnisbet
+ * 
+ */
+public class BlogMVC implements ModelViewController
+{
+
+	private Blog					blog;
+	private PointOfInterestActivity	poi;
+
+	public BlogMVC( PointOfInterestActivity activity, Blog blog )
+	{
+		this.blog = blog;
+		this.poi = activity;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see path.wiser.mobile.ModelViewController#update()
+	 */
+	@Override
+	public void update()
+	{
+		if (this.blog != null)
+		{
+			// take the values from the blog and write them to screen
+			TextView textView = (TextView) poi.findViewById( R.id.Poi_Title );
+			textView.setText( blog.getPoiTitle() );
+			textView = (TextView) poi.findViewById( R.id.Poi_Blog );
+			textView.setText( blog.getDescription() );
+			textView = (TextView) poi.findViewById( R.id.Poi_Tag );
+			Tags tag = blog.getTags();
+			textView.setText( tag.toString() );
+		}
+		else
+		{
+			Log.e( this.getClass().getName(), "Unable to update because blog is null." );
+		}
+		// TODO also include images.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see path.wiser.mobile.ModelViewController#change()
+	 */
+	@Override
+	public void change()
+	{
+		// TODO take the values from the screen and put them in the blog
+		if (this.blog != null)
+		{
+			TextView textView = (TextView) poi.findViewById( R.id.Poi_Title );
+			blog.setPoiTitle( (String) textView.getText() );
+			textView = (TextView) poi.findViewById( R.id.Poi_Blog );
+			blog.setDescription( (String) textView.getText() );
+			textView = (TextView) poi.findViewById( R.id.Poi_Tag );
+			blog.setTags( (String) textView.getText() );
+			// TODO also include images.
+		}
+		else
+		{
+			Log.e( this.getClass().getName(), "Unable to update because blog is null." );
+		}
+	}
+
+}
---
+++ WiserPathMobile/src/path/wiser/mobile/ModelViewController.java
@@ -0,0 +1,15 @@
+package path.wiser.mobile;
+
+public interface ModelViewController
+{
+	/**
+	 * Update the view.
+	 */
+	public abstract void update();
+
+	/**
+	 * Change the model.
+	 */
+	public abstract void change();
+
+}
