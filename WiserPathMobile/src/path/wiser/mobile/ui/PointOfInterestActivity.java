7c8eeadd0afdd5de3edd0327df3502433003eb60
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-17 22:52:17 +0000

    Added and tested MVC for POI

--- WiserPathMobile/src/path/wiser/mobile/ui/PointOfInterestActivity.java
+++ WiserPathMobile/src/path/wiser/mobile/ui/PointOfInterestActivity.java
@@ -5,12 +5,14 @@ package path.wiser.mobile.ui;
 
 import java.util.Vector;
 
+import path.wiser.mobile.BlogMVC;
 import path.wiser.mobile.R;
 import path.wiser.mobile.geo.Blog;
 import path.wiser.mobile.geo.GPS;
 import path.wiser.mobile.services.HTTPService;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -45,9 +47,19 @@ public class PointOfInterestActivity extends Selectable
 		super.onCreate( savedInstanceState );
 		// set content view so you can grab stuff in it.
 		setContentView( R.layout.poi_tab );
-		// GPS gps = new GPS( this );
+		gps = new GPS( this );
 		this.blogs = new Vector<Blog>();
-		// this.blog = new Blog();
+		this.blog = new Blog();
+
+		this.blog.setPoiTitle( "Andrew's test blog" );
+
+		// test code
+		BlogMVC mvc = new BlogMVC( this, this.blog );
+		mvc.update();
+		this.blog.setDescription( "Oh, how nice, a new blog!" );
+		mvc.update();
+
+		// test code
 
 		// add a onClick listener to the text screens so we can remove the
 		// existing text and allow the user to start entering text.
@@ -144,24 +156,66 @@ public class PointOfInterestActivity extends Selectable
 	protected void save()
 	{
 		// TODO Serialize the entire vector to disk.
+		BlogMVC mvc = new BlogMVC( this, this.blog );
+		mvc.change();
+		if (isSerialized())
+		{
+			text = String.format( res.getString( R.string.poi_blog_save_success_msg ) );
+		}
+	}
+
+	private boolean isSerialized()
+	{
+		return true;
 	}
 
 	@Override
 	protected void upload()
 	{
-		// TODO Auto-generated method stub
-		HTTPService service = HTTPService.getInstance();
-		if (service.uploadBlog( this.blog ))
+		// Make sure the blog is ready to go.
+		if (this.blog != null && this.blog.validate())
 		{
-			;
+			// get the HTTPService for posting data.
+			HTTPService service = HTTPService.getInstance();
+			// upload the blog.
+			if (service.uploadBlog( this.blog ))
+			{
+				text = String.format( res.getString( R.string.poi_blog_post_success_msg ) );
+			}
+			else
+			{
+				text = String.format( res.getString( R.string.poi_blog_post_fail_msg ) );
+			}
 		}
+		else
+		{
+			text = String.format( res.getString( R.string.poi_blog_post_invalid_msg ) );
+		}
+		msg = Html.fromHtml( text );
+		showMessage( msg );
 	}
 
+	// Occurs when ever you move away from the screen. All objects that were created for
+	// this activity have persistence but are suspended.
 	@Override
 	public void onPause()
 	{
 		// TODO store data before we move away from this screen.
 		super.onPause();
+		this.print( "onPause() called!" );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	public void onResume()
+	{
+		//
+		super.onResume();
+		this.print( "onResume() called!" );
 	}
 
 	@Override
@@ -169,10 +223,15 @@ public class PointOfInterestActivity extends Selectable
 	{
 		// TODO if a location has already been stored stop the GPS
 		// we do this so the location doesn't keep updating after you leave the POI.
-		if (this.blog.needsLocation())
+		if (this.blog != null && this.blog.needsLocation())
 		{
 			this.blog.setLocation( location );
-			// this.gps = null;
+			if (location != null)
+			{
+				this.print( "LOCATION CHANGED " + location.getLongitude() + " long " + location.getLatitude() + " lat." );
+			}
+			// turn off the gps.
+			this.gps = null;
 		}
 	}
 }
