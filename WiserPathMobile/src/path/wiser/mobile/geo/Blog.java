efe36213202a8c9970101a7a2ababece1c32a75c
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-06-17 22:53:41 +0000

    Implemented methods to handle tags and removed tags as a field from POI. Made the getter and setter for tags into abstract.

--- WiserPathMobile/src/path/wiser/mobile/geo/Blog.java
+++ WiserPathMobile/src/path/wiser/mobile/geo/Blog.java
@@ -3,6 +3,7 @@
  */
 package path.wiser.mobile.geo;
 
+import path.wiser.mobile.Tags;
 import android.location.Location;
 
 /**
@@ -15,8 +16,13 @@ public class Blog extends POI
 {
 	// a blog has a single location.
 	private Location	location	= null;
+	private Tags		tags		= null;
 
 	// TODO include image
+	public Blog()
+	{
+		this.tags = new Tags();
+	}
 
 	/**
 	 * @param location the location to set
@@ -27,11 +33,31 @@ public class Blog extends POI
 	}
 
 	/**
-	 * @return True if the blog has a stored location and false otherwise.
+	 * @return True if the description has a stored location and false otherwise.
 	 */
 	public boolean needsLocation()
 	{
 		return this.location == null;
 	}
 
+	/**
+	 * This method ensures that the minimal information for a POI in Wiser Path
+	 * has been met. The GeoBlog must have a title, location and text body.
+	 */
+	public boolean validate()
+	{
+		return this.title.length() > 0 && this.description.length() > 0 && ( needsLocation() == false );
+	}
+
+	@Override
+	public Tags getTags()
+	{
+		return this.tags;
+	}
+
+	@Override
+	public void setTags( String tags )
+	{
+		this.tags.setTags( tags );
+	}
 }
