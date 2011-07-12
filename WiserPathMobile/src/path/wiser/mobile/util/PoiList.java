4633d625766dcb32892486f19a2e6b8019cf75f0
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-07-12 05:45:27 +0000

    Fixing new Trace or Blog.

--- WiserPathMobile/src/path/wiser/mobile/util/PoiList.java
+++ WiserPathMobile/src/path/wiser/mobile/util/PoiList.java
@@ -18,10 +18,9 @@ public class POIList
 		BLOG, TRACE
 	}
 
-	@SuppressWarnings("rawtypes")
-	private CircularList	list		= null;
-	private POI				currentPoi	= null;
-	private Type			myType;
+	private CircularList<POI>	list		= null;
+	private POI					currentPoi	= null;
+	private Type				myType;
 
 	/**
 	 * @param type the type of list to create.
@@ -32,20 +31,18 @@ public class POIList
 		switch (type)
 		{
 		case TRACE:
-			this.list = new CircularList<Trace>();
 			this.currentPoi = new Trace();
 			break;
 		default:
-			this.list = new CircularList<Blog>();
 			this.currentPoi = new Blog();
 		}
+		this.list = new CircularList<POI>();
 		this.list.pushTail( this.currentPoi );
 	}
 
 	/**
 	 * @return the previous POI object (cast it to something useful).
 	 */
-	@SuppressWarnings("unchecked")
 	public POI previous()
 	{
 		// if the currentBlog is not null push it on the tail and get the head.
@@ -56,14 +53,28 @@ public class POIList
 			{
 			case TRACE:
 				this.currentPoi = new Trace();
+				this.list.pushTail( this.currentPoi );
+				this.currentPoi = (Trace) this.list.popHead();
 				break;
 			default:
 				this.currentPoi = new Blog();
+				this.list.pushTail( this.currentPoi );
+				this.currentPoi = (Blog) this.list.popHead();
 			}
 		}
-
-		this.list.pushTail( this.currentPoi );
-		this.currentPoi = (POI) this.list.popHead();
+		else
+		{
+			switch (this.myType)
+			{
+			case TRACE:
+				this.list.pushTail( this.currentPoi );
+				this.currentPoi = (Trace) this.list.popHead();
+				break;
+			default:
+				this.list.pushTail( this.currentPoi );
+				this.currentPoi = (Blog) this.list.popHead();
+			}
+		}
 
 		return this.currentPoi;
 	}
@@ -71,7 +82,6 @@ public class POIList
 	/**
 	 * @return The next POI object (cast it to something useful).
 	 */
-	@SuppressWarnings("unchecked")
 	public POI next()
 	{
 		// if the currentBlog is not null push it on the tail and get the head.
@@ -82,14 +92,28 @@ public class POIList
 			{
 			case TRACE:
 				this.currentPoi = new Trace();
+				this.list.pushHead( this.currentPoi );
+				this.currentPoi = (Trace) this.list.popTail();
 				break;
 			default:
 				this.currentPoi = new Blog();
+				this.list.pushHead( this.currentPoi );
+				this.currentPoi = (Blog) this.list.popTail();
 			}
 		}
-
-		this.list.pushHead( this.currentPoi );
-		this.currentPoi = (POI) this.list.popTail();
+		else
+		{
+			switch (this.myType)
+			{
+			case TRACE:
+				this.list.pushHead( this.currentPoi );
+				this.currentPoi = (Trace) this.list.popTail();
+				break;
+			default:
+				this.list.pushHead( this.currentPoi );
+				this.currentPoi = (Blog) this.list.popTail();
+			}
+		}
 
 		return this.currentPoi;
 	}
