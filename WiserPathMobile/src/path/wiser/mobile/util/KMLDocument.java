be5787453060f8019aff11619458803443fd3ed2
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-07-14 23:52:58 +0000

    created but not tested Blog and Trace serialization

--- WiserPathMobile/src/path/wiser/mobile/util/KMLDocument.java
+++ WiserPathMobile/src/path/wiser/mobile/util/KMLDocument.java
@@ -23,6 +23,7 @@ import org.w3c.dom.Text;
 
 import path.wiser.mobile.geo.Blog;
 import path.wiser.mobile.geo.POI;
+import path.wiser.mobile.geo.Trace;
 
 /**
  * @author andrewnisbet
@@ -30,8 +31,10 @@ import path.wiser.mobile.geo.POI;
  */
 public class KMLDocument
 {
-	private Document	doc		= null;
-	private Element		docRoot	= null;
+	private static final String	LINE_TYPE		= "userLineType";
+	private Document			doc				= null;
+	private Element				docRoot			= null;
+	private boolean				includeStyle	= true;
 
 	public KMLDocument()
 	{
@@ -114,8 +117,119 @@ public class KMLDocument
 
 	private void outputTrace( POI poi )
 	{
-		// TODO Auto-generated method stub
+		// <?xml version="1.0" encoding="UTF-8"?>
+		// <kml xmlns="http://www.opengis.net/kml/2.2">
+		// <Document>
+		// <Style id="userLineType">
+		// <LineStyle>
+		// <color>7f00ffff</color>
+		// <width>1</width>
+		// </LineStyle>
+		// <PolyStyle>
+		// <color>7f00ffff</color>
+		// </PolyStyle>
+		// </Style>
+		// <Placemark>
+		// <name>My Favourite Trace</name>
+		// <description>Best Route through River Valley</description>
+		// <styleUrl>#userLineType</styleUrl>
+		// <LineString>
+		// <altitudeMode>relativeToGround</altitudeMode>
+		// <!-- tack on ',<altitude>' for traces with altitude -->
+		// <coordinates> -112.2550785337791,36.07954952145647
+		// -112.2549277039738,36.08117083492122
+		// -112.2552505069063,36.08260761307279
+		// -112.2564540158376,36.08395660588506
+		// -112.2580238976449,36.08511401044813
+		// -112.2595218489022,36.08584355239394
+		// -112.2608216347552,36.08612634548589
+		// -112.262073428656,36.08626019085147
+		// -112.2633204928495,36.08621519860091
+		// -112.2644963846444,36.08627897945274
+		// -112.2656969554589,36.08649599090644
+		// </coordinates>
+		// </LineString>
+		// </Placemark>
+		// </Document>
+		// </kml>
+		if (this.includeStyle)
+		{
+			this.docRoot.appendChild( getTraceStyle() );
+			this.includeStyle = false;
+		}
+		Element placeMark = doc.createElement( "Placemark" );
+		// add the title of the trace.
+		placeMark.appendChild( getName( poi ) );
+		placeMark.appendChild( getDescription( poi ) );
+		// add the styling for the line type.
+		Element styleUrl = doc.createElement( "styleUrl" );
+		Text text = doc.createTextNode( "#" + LINE_TYPE );
+		styleUrl.appendChild( text );
+		placeMark.appendChild( styleUrl );
 
+		Element lineString = doc.createElement( "LineString" );
+		// Altitude mode
+		Element altitudeMode = doc.createElement( "altitudeMode" );
+		text = doc.createTextNode( "relativeToGround" );
+		altitudeMode.appendChild( text );
+		lineString.appendChild( altitudeMode );
+		// now the coordinates
+		Element coordinates = doc.createElement( "coordinates" );
+		coordinates.appendChild( getCoordinates( (Trace) poi ) );
+		lineString.appendChild( coordinates );
+		placeMark.appendChild( lineString );
+
+		this.docRoot.appendChild( placeMark );
+	}
+
+	private Node getCoordinates( Trace poi )
+	{
+		// -112.2550785337791,36.07954952145647
+		// -112.2549277039738,36.08117083492122
+		// -112.2552505069063,36.08260761307279
+		// ...
+		Text text = doc.createTextNode( poi.getCoordinates() );
+
+		return text;
+	}
+
+	/**
+	 * Note that you need only call this once per document.
+	 * 
+	 * @return a line style for traces.
+	 */
+	private Node getTraceStyle()
+	{
+		// <Style id="userLineType">
+		// <LineStyle>
+		// <color>7f00ffff</color>
+		// <width>1</width>
+		// </LineStyle>
+		// <PolyStyle>
+		// <color>7f00ffff</color>
+		// </PolyStyle>
+		// </Style>
+		Element style = doc.createElement( "Style" );
+		style.setAttribute( "id", LINE_TYPE );
+		Element lineStyle = doc.createElement( "LineStyle" );
+		Element colour = doc.createElement( "color" );
+		Text text = doc.createTextNode( "7f00ffff" );
+		colour.appendChild( text );
+		lineStyle.appendChild( colour );
+		Element width = doc.createElement( "width" );
+		text = doc.createTextNode( "1" );
+		width.appendChild( text );
+		lineStyle.appendChild( width );
+		style.appendChild( lineStyle );
+		// now for the PolyStyle
+		Element polyStyle = doc.createElement( "PolyStyle" );
+		colour = doc.createElement( "color" );
+		text = doc.createTextNode( "7f00ffff" );
+		colour.appendChild( text );
+		polyStyle.appendChild( colour );
+		style.appendChild( style );
+
+		return style;
 	}
 
 	/**
@@ -156,6 +270,9 @@ public class KMLDocument
 		return title;
 	}
 
+	/**
+	 * Writes the XML tree to media.
+	 */
 	public void write()
 	{
 		// Output the XML
@@ -183,9 +300,22 @@ public class KMLDocument
 		{
 			e.printStackTrace();
 		}
-
+		// TODO write XML to somewhere more useful.
 		System.out.println( sw.toString() );
 
+	}
+
+	/**
+	 * Retrieves the list from disk.
+	 * 
+	 * @param poiList
+	 * @return true if the operation was successful and false otherwise.
+	 */
+	public boolean deserialize( PoiList poiList )
+	{
+		// read the file and parse it into an XML tree.
+		// get the elements and add them to the poiList.
+		return false;
 	}
 
 }
