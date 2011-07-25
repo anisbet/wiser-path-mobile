c403070760ed78382541fee13dabf2fa8a81b405
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-07-25 21:14:49 +0000

    New type extending Blog

---
+++ WiserPathMobile/src/path/wiser/mobile/geo/Incident.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package path.wiser.mobile.geo;
+
+/**
+ * @author andrewnisbet
+ * 
+ */
+public class Incident extends Blog
+{
+
+	@Override
+	public boolean isIncident()
+	{
+		return true;
+	}
+
+}
