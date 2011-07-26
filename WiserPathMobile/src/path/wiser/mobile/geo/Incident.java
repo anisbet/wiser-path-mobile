c1c1f6c0d6497a41865cd754f526ed2402d136a5
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-07-26 19:54:21 +0000

    Added a copy constructor.

--- WiserPathMobile/src/path/wiser/mobile/geo/Incident.java
+++ WiserPathMobile/src/path/wiser/mobile/geo/Incident.java
@@ -10,6 +10,21 @@ package path.wiser.mobile.geo;
 public class Incident extends Blog
 {
 
+	/**
+	 * Creates a new Incident object from an existing Blog object.
+	 * 
+	 * @param poi
+	 */
+	public Incident( Blog poi )
+	{
+		this.setType( POI.Type.INCIDENT );
+		// you do this when you read the extended data and discover you are recording a Incident instead of a Blog.
+		this.description = poi.description;
+		this.isUploaded = poi.isUploaded;
+		this.tags = poi.tags;
+		this.title = poi.title;
+	}
+
 	@Override
 	public boolean isIncident()
 	{
