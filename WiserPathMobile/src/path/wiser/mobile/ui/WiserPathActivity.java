e43e6ca97d968693f0cc00a5acc9ec6377fcb259
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-08-03 17:47:54 +0000

    Added POI icon to the google map tab.

--- WiserPathMobile/src/path/wiser/mobile/ui/WiserPathActivity.java
+++ WiserPathMobile/src/path/wiser/mobile/ui/WiserPathActivity.java
@@ -1,13 +1,25 @@
 package path.wiser.mobile.ui;
 
+import java.util.List;
+
 import path.wiser.mobile.R;
+import path.wiser.mobile.geo.MapOverlayItems;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class WiserPathActivity extends MapActivity
 {
+
+	private List<Overlay>	mapOverlays;
+	private Drawable		drawable;
+	private MapOverlayItems		itemizedOverlay;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate( Bundle savedInstanceState )
@@ -16,6 +28,17 @@ public class WiserPathActivity extends MapActivity
 		setContentView( R.layout.wiserpath_tab );
 		MapView mapView = (MapView) findViewById( R.id.mapview );
 		mapView.setBuiltInZoomControls( true );
+
+		// now instantiate the map's overlay items.
+		mapOverlays = mapView.getOverlays();
+		drawable = this.getResources().getDrawable( R.drawable.ic_tee_poi_blue );
+		itemizedOverlay = new MapOverlayItems( drawable );
+
+		// this places a Wiser path POI 'tee' icon on west ed mall.
+		GeoPoint point = new GeoPoint( 53522780, -113623052 ); // WGS84 * 1e6
+		OverlayItem overlayitem = new OverlayItem( point, "West Edmonton Mall", "The greatest indoor show on Earth!" );
+		itemizedOverlay.addOverlay( overlayitem );
+		mapOverlays.add( itemizedOverlay );
 	}
 
 	@Override
