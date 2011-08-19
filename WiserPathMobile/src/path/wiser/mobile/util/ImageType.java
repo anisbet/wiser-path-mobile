fec7da19dced51fcf76d66e6a59e19bee07b7a85
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-08-19 22:33:55 +0000

    Added a clever toString Method to the enum.

--- WiserPathMobile/src/path/wiser/mobile/util/ImageType.java
+++ WiserPathMobile/src/path/wiser/mobile/util/ImageType.java
@@ -9,5 +9,18 @@ package path.wiser.mobile.util;
  */
 public enum ImageType
 {
-	UNSUPPORTED, JPG,
+	UNSUPPORTED, JPG; // needed to delineate toString method.
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Enum#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		// send the lower case version of the enum
+		String s = super.toString();
+		return "." + s.toLowerCase();
+	}
 }
