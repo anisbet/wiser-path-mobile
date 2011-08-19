3e772983d4d1b525ecbfb50e21b4279759a415cb
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-08-19 22:39:21 +0000

    Added code to not redraw the thumbnail over and over if the only thing that changes is the text.

--- WiserPathMobile/src/path/wiser/mobile/ui/BlogMVC.java
+++ WiserPathMobile/src/path/wiser/mobile/ui/BlogMVC.java
@@ -24,13 +24,13 @@ public class BlogMVC implements ModelViewController
 {
 
 	private Blog					blog;
-	private PointOfInterestActivity	poi;
-	private Intent					data;
+	private PointOfInterestActivity	activity;
+	private Intent					cameraData; // used to retrieve images from the camera activity Intent.
 
 	public BlogMVC( PointOfInterestActivity activity, Blog blog )
 	{
 		this.blog = blog;
-		this.poi = activity;
+		this.activity = activity;
 	}
 
 	/*
@@ -44,17 +44,17 @@ public class BlogMVC implements ModelViewController
 		if (this.blog != null)
 		{
 			// take the values from the blog and write them to screen
-			TextView textView = (TextView) poi.findViewById( R.id.Poi_Title );
+			TextView textView = (TextView) activity.findViewById( R.id.Poi_Title );
 			textView.setText( blog.getTitle() );
-			textView = (TextView) poi.findViewById( R.id.Poi_Blog );
+			textView = (TextView) activity.findViewById( R.id.Poi_Blog );
 			textView.setText( blog.getDescription() );
-			textView = (TextView) poi.findViewById( R.id.Poi_Tag );
+			textView = (TextView) activity.findViewById( R.id.Poi_Tag );
 			Tags tag = blog.getTags();
 			textView.setText( tag.toString() );
 			// now set the image preview.
 			MediaReader mediaReader = new MediaReader();
 			Bitmap thumbnail = mediaReader.readImageFile( WPEnvironment.BLOG_PATH, this.blog.getImageName() );
-			ImageView imageView = (ImageView) this.poi.findViewById( R.id.photo_preview );
+			ImageView imageView = (ImageView) this.activity.findViewById( R.id.photo_preview );
 			if (thumbnail != null)
 			{
 				imageView.setImageBitmap( thumbnail );
@@ -81,26 +81,29 @@ public class BlogMVC implements ModelViewController
 		// take the values from the screen and put them in the blog
 		// this is called from a request to change the underlying model because
 		// the user entered new data or navigated away from the this POI.
-		if (this.blog != null)
+		if (blog != null)
 		{
-			TextView textView = (TextView) poi.findViewById( R.id.Poi_Title );
+			TextView textView = (TextView) activity.findViewById( R.id.Poi_Title );
 			blog.setTitle( textView.getText().toString() );
-			textView = (TextView) poi.findViewById( R.id.Poi_Blog );
+			textView = (TextView) activity.findViewById( R.id.Poi_Blog );
 			blog.setDescription( textView.getText().toString() );
-			textView = (TextView) poi.findViewById( R.id.Poi_Tag );
+			textView = (TextView) activity.findViewById( R.id.Poi_Tag );
 			blog.setTags( new Tags( textView.getText().toString() ) );
 			// also include images.
 			// the model can also change if the host activity received an Intent from the Camera activity.
 			// in that case we need to save the image and update the blogs imageName.
-			if (data != null) // this action was the result of an Intent ie image from the camera.
+			if (isFreshImage()) // this action was the result of an Intent ie image from the camera.
 			{
 				// now save the image for the PointOfInterestActivity.
-				Bitmap image = (Bitmap) data.getExtras().get( "data" );
+				Bitmap image = (Bitmap) cameraData.getExtras().get( "data" );
 				MediaWriter mediaWriter = new MediaWriter();
 				String fileName = getImageFileName();
 				mediaWriter.writeImageFile( WPEnvironment.BLOG_PATH, fileName, image );
 				// now update the name of the image in the POI
-				this.blog.setImageName( fileName );
+				blog.setImageName( fileName );
+				// reset the reference to the camera data so we don't keep reloading
+				// a thumbnail every time the user enters new text.
+				cameraData = null;
 			}
 
 		}
@@ -111,11 +114,24 @@ public class BlogMVC implements ModelViewController
 	}
 
 	/**
-	 * @param data the data to set
+	 * This method will return true if data was returned from the camera activity. The result
+	 * from the camera may be cancel but it was called and terminated with a non-crashing event.
+	 * Classes that use this class should check that the result was ok before creating a model view controller.
+	 * {@link path.wiser.mobile.ui.PointOfInterestActivity} is designed to check for success of image capture.
+	 * 
+	 * @return true if the camera activity has finished and false otherwise.
+	 */
+	private boolean isFreshImage()
+	{
+		return cameraData != null;
+	}
+
+	/**
+	 * @param cameraData the cameraData to set
 	 */
 	public final void setData( Intent data )
 	{
-		this.data = data;
+		this.cameraData = data;
 	}
 
 	/**
@@ -134,7 +150,8 @@ public class BlogMVC implements ModelViewController
 		// }
 		// else
 		// {
-		return String.valueOf( System.currentTimeMillis() ) + ".jpg"; // for now give it a number.
+		// for now give it a number.
+		return String.valueOf( System.currentTimeMillis() ) + WPEnvironment.getImageExtension();
 		// }
 	}
 
