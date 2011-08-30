3b26bb3f8516383c6968a828b31cfc21290a9aa6
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-08-30 22:41:34 +0000

    Initial commit of Polylines for Google map.

---
+++ WiserPathMobile/src/path/wiser/mobile/geo/WPMapLayerPolyline.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package path.wiser.mobile.geo;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * At the time of the writing of this application Google does not have a polyline API for Android
+ * and much of the research that I found avoided the topic by giving solutions in Javascript. There is an
+ * API for that. Anyway this draws points along the line between two POIs using Bresenham (1962).
+ * 
+ * @author andrew nisbet
+ * 
+ */
+public class WPMapLayerPolyline extends WPMapLayerPoints
+{
+
+	private static final int	FIRST_ITEM_INDEX	= 0;
+
+	/**
+	 * @param defaultMarker
+	 * @param type
+	 */
+	public WPMapLayerPolyline( Drawable defaultMarker, MapLayerType type )
+	{
+		super( defaultMarker, type ); // there is error of 2 px because the super class binds icon bottom center.
+	}
+
+	@Override
+	public void addOverlayItem( OverlayItem item )
+	{
+		int last = overlayItems.size() - 1;
+		// get the last item on the list and if there is one fill the space between with points
+		// to make it look like a line.
+		if (last >= FIRST_ITEM_INDEX)
+		{
+			OverlayItem lastItem = overlayItems.get( last );
+			line( lastItem, item );
+		}
+		else
+		{
+			super.addOverlayItem( item );
+		}
+	}
+
+	private void line( OverlayItem previousPoint, OverlayItem currentPoint )
+	{
+		// compute diagonal between two point with reasonable zoom factor so we don't draw too many points.
+		// for now just draw a bunch of points.
+		int x0 = previousPoint.getPoint().getLatitudeE6();
+		int y0 = previousPoint.getPoint().getLongitudeE6();
+		int x1 = currentPoint.getPoint().getLatitudeE6();
+		int y1 = currentPoint.getPoint().getLongitudeE6();
+		// perform Bresenham (1962).
+		int dx = Math.abs( x1 - x0 );
+		int dy = Math.abs( y1 - y0 );
+		int sx, sy;
+		if (x0 < x1)
+		{
+			sx = 1;
+		}
+		else
+		{
+			sx = -1;
+		}
+
+		if (y0 < y1)
+		{
+			sy = 1;
+		}
+		else
+		{
+			sy = -1;
+		}
+
+		int error = dx - dy;
+
+		while (true)
+		{
+			setPixel( x0, y0 );
+
+			if (( x0 == x1 ) && ( y0 == y1 ))
+			{
+				break;
+			}
+			int e2 = 2 * error;
+			if (e2 > -dy)
+			{
+				error = error - dy;
+				x0 = x0 + sx;
+			}
+			if (e2 < dx)
+			{
+				error = error + dx;
+				y0 = y0 + sy;
+			}
+		}
+	}
+
+	private void setPixel( int x, int y )
+	{
+		GeoPoint point = new GeoPoint( x, y );
+		super.addOverlayItem( new OverlayItem( point, "", "" ) );
+	}
+}
