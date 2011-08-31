654a236adc3a525c51be97aee838c2d48e57bf7a
nisbetorama <nisbetorama@967e84d7-32c7-60fc-215e-7f1a3bb5b9e4>
2011-08-31 22:19:08 +0000

    Added comments and commented out the drawline function so we can take another look at it.

--- WiserPathMobile/src/path/wiser/mobile/geo/WPMapLayerPolyline.java
+++ WiserPathMobile/src/path/wiser/mobile/geo/WPMapLayerPolyline.java
@@ -5,7 +5,6 @@ package path.wiser.mobile.geo;
 
 import android.graphics.drawable.Drawable;
 
-import com.google.android.maps.GeoPoint;
 import com.google.android.maps.OverlayItem;
 
 /**
@@ -39,7 +38,7 @@ public class WPMapLayerPolyline extends WPMapLayerPoints
 		if (last >= FIRST_ITEM_INDEX)
 		{
 			OverlayItem lastItem = overlayItems.get( last );
-			line( lastItem, item );
+			drawLine( lastItem, item );
 		}
 		else
 		{
@@ -47,63 +46,78 @@ public class WPMapLayerPolyline extends WPMapLayerPoints
 		}
 	}
 
-	private void line( OverlayItem previousPoint, OverlayItem currentPoint )
+	private void drawLine( OverlayItem previousPoint, OverlayItem currentPoint )
 	{
-		// compute diagonal between two point with reasonable zoom factor so we don't draw too many points.
-		// for now just draw a bunch of points.
-		int x0 = previousPoint.getPoint().getLatitudeE6();
-		int y0 = previousPoint.getPoint().getLongitudeE6();
-		int x1 = currentPoint.getPoint().getLatitudeE6();
-		int y1 = currentPoint.getPoint().getLongitudeE6();
-		// perform Bresenham (1962).
-		int dx = Math.abs( x1 - x0 );
-		int dy = Math.abs( y1 - y0 );
-		int sx, sy;
-		if (x0 < x1)
-		{
-			sx = 1;
-		}
-		else
-		{
-			sx = -1;
-		}
 
-		if (y0 < y1)
-		{
-			sy = 1;
-		}
-		else
-		{
-			sy = -1;
-		}
-
-		int error = dx - dy;
-
-		while (true)
-		{
-			setPixel( x0, y0 );
-
-			if (( x0 == x1 ) && ( y0 == y1 ))
-			{
-				break;
-			}
-			int e2 = 2 * error;
-			if (e2 > -dy)
-			{
-				error = error - dy;
-				x0 = x0 + sx;
-			}
-			if (e2 < dx)
-			{
-				error = error + dx;
-				y0 = y0 + sy;
-			}
-		}
 	}
 
-	private void setPixel( int x, int y )
-	{
-		GeoPoint point = new GeoPoint( x, y );
-		super.addOverlayItem( new OverlayItem( point, "", "" ) );
-	}
+	// //////////////////////// Naive Implementation with points. //////////////////////////////////
+
+	// /**
+	// * Draws a line of GeoPoints on the map because the Android API doesn't support
+	// * polylines as of version 8 of Google API. It uses the Bresenham optimized
+	// * line drawing algorithm.
+	// *
+	// * @param previousPoint
+	// * @param currentPoint
+	// */
+	// private void drawLine( OverlayItem previousPoint, OverlayItem currentPoint )
+	// {
+	// // compute diagonal between two point with reasonable zoom factor so we don't draw too many points.
+	// // for now just draw a bunch of points.
+	// int x0 = previousPoint.getPoint().getLatitudeE6();
+	// int y0 = previousPoint.getPoint().getLongitudeE6();
+	// int x1 = currentPoint.getPoint().getLatitudeE6();
+	// int y1 = currentPoint.getPoint().getLongitudeE6();
+	// // perform Bresenham (1962).
+	// int dx = Math.abs( x1 - x0 );
+	// int dy = Math.abs( y1 - y0 );
+	// int sx, sy;
+	// if (x0 < x1)
+	// {
+	// sx = 1;
+	// }
+	// else
+	// {
+	// sx = -1;
+	// }
+	//
+	// if (y0 < y1)
+	// {
+	// sy = 1;
+	// }
+	// else
+	// {
+	// sy = -1;
+	// }
+	//
+	// int error = dx - dy;
+	//
+	// while (true)
+	// {
+	// setPixel( x0, y0 );
+	//
+	// if (( x0 == x1 ) && ( y0 == y1 ))
+	// {
+	// break;
+	// }
+	// int e2 = 2 * error;
+	// if (e2 > -dy)
+	// {
+	// error = error - dy;
+	// x0 = x0 + sx;
+	// }
+	// if (e2 < dx)
+	// {
+	// error = error + dx;
+	// y0 = y0 + sy;
+	// }
+	// }
+	// }
+	//
+	// private void setPixel( int x, int y )
+	// {
+	// GeoPoint point = new GeoPoint( x, y );
+	// super.addOverlayItem( new OverlayItem( point, "", "" ) );
+	// }
 }
